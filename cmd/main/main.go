f1dfa72bc8714460f111f165acece3bc8b411da3
Sai Shashaank V V <archdemon.developer@gmail.com>
2026-08-29 15:28:55 +0530

    feat(dependencies): added all needed dependencies for inital phases

---
+++ cmd/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	_ "github.com/gin-gonic/gin"
+	_ "github.com/golang-migrate/migrate/v4"
+	_ "github.com/google/uuid"
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/shopspring/decimal"
+	_ "github.com/stretchr/testify/assert"
+)
+
+func main() {
+	// stub
+}
