dda24f84c9000de8c5a17aaa74147a101b0fdbbf
Sai Shashaank V V <archdemon.developer@gmail.com>
2026-08-31 22:11:00 +0530

    feat(api): add main entry point with health endpoint

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	_ "github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	_ "github.com/golang-migrate/migrate/v4"
 	_ "github.com/google/uuid"
 	_ "github.com/jackc/pgx/v5"
@@ -10,5 +13,14 @@ import (
 )
 
 func main() {
-	// stub
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		fmt.Println("GET - /health - Checking application up status")
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	router.Run()
 }
